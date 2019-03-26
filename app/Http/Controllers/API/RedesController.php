c64ffd99cfa221504732a85da938b2ea12f31bf2
thomasklz <pipos_rgt@hotmail.com>
2019-03-25 23:15:40 -0500

    compartir redes update

--- app/Http/Controllers/API/RedesController.php
+++ app/Http/Controllers/API/RedesController.php
@@ -16,7 +16,7 @@ class RedesController extends Controller
      */
     public function index()
     {
-        $redes= Redes::with('user:id,user')->orderBy('id', 'DESC')->get();
+        $redes= Redes::orderBy('id', 'DESC')->with('user:id,user')->get();
         return response()->json(['redes'=>$redes], 200);
     }
 
