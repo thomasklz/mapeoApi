88c2fe36c1184186613b8a695f6ca8f58a1f0d12
thomasklz <pipos_rgt@hotmail.com>
2019-03-29 00:02:47 -0500

    imagen

---
+++ public/api/uploadPhoto.php
@@ -0,0 +1,19 @@
+<?php
+    $target_dir = "images/";
+	$target_file = $target_dir . basename($_FILES["photo"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+	$check = getimagesize($_FILES["photo"]["tmp_name"]);
+	if($check !== false) {
+		echo "File is an image - " . $check["mime"] . ".";
+		$uploadOk = 1;
+		if (move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)) {
+			echo "The file ". basename( $_FILES["photo"]["name"]). " has been uploaded.";
+		} else {
+			echo "Sorry, there was an error uploading your file.";
+		}
+	} else {
+		echo "File is not an image.";
+		$uploadOk = 0;
+	}
+?>
