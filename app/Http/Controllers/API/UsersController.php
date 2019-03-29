845271d4da37ca250dd5de16e5782a339ac3ebed
thomasklz <pipos_rgt@hotmail.com>
2019-03-28 20:02:51 -0500

    imagen

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
+use Illuminate\Http\UploadedFile;
 use App\Http\Controllers\Controller;
 use App\Users;
 use Response;
