f27eb52cf1fc71064595585bec737b7304c6c191
thomasklz <pipos_rgt@hotmail.com>
2019-04-03 20:58:00 -0500

    obtener coordenadas radio

--- app/Redes.php
+++ app/Redes.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\DB;
 class Redes extends Model
 {
    protected $table = 'redes';
@@ -15,4 +15,10 @@ class Redes extends Model
     public  function user (){
       return $this->belongsTo(Users::class, 'idUser');
     }
+public static function getByDistance($lat, $lng, $distance)
+{
+  $results = DB::select(DB::raw('SELECT id, ( 3959 * acos( cos( radians(' . $lat . ') ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(' . $lng . ') ) + sin( radians(' . $lat .') ) * sin( radians(lat) ) ) ) AS distance FROM articles HAVING distance < ' . $distance . ' ORDER BY distance') );
+
+  return $results;
+}
 }
